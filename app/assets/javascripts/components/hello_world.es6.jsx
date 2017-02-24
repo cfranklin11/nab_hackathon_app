7deaf007b306fe1a9f697ee2cbc45e5e92c3c762
melissa gattoni <meligatt@gmail.com>
2017-02-24 14:07:45 +1100

    add form

--- app/assets/javascripts/components/hello_world.es6.jsx
+++ app/assets/javascripts/components/hello_world.es6.jsx
@@ -11,10 +11,34 @@ class HelloWorld extends React.Component {
 
   render () {
     return (
-      <div>
-        <div>Path: {this.props.path}</div>
-        <button onClick={ this.handleClick }>Click Me</button>
+    <div className="col-xs-12 col-md-8">
+      <form>
+      <div className="form-group">
+        <label htmlFor="ammount">How much money you want to spend today?</label>
+        <input type="ammount" className="form-control" id="exampleInputEmail1" placeholder="Ammount" />
       </div>
+      <div className="form-group">
+      <label htmlFor="currency">Enter your currency</label>
+      // <input type="currency" className="form-control" id="exampleInputPassword1" placeholder="Your currency"/>
+      <div className="dropdown">
+      <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+      Dropdown
+      <span className="caret"></span>
+      </button>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+      <li><a href="#">Action</a></li>
+      <li><a href="#">Another action</a></li>
+      <li><a href="#">Something else here</a></li>
+      <li role="separator" className="divider"></li>
+      <li><a href="#">Separated link</a></li>
+      </ul>
+      </div>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
+
     );
   }
 }
